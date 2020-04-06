af9736de1c22faef5391f308ab4e83915c208881
edo lorenza <edolrnz@gmail.com>
2020-04-06 14:56:04 +0700

    Initial Commit

---
+++ timer-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer-app
+//
+//  Created by Edo Lorenza on 06/04/20.
+//  Copyright © 2020 Edo Lorenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
